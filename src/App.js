c3acfbf769a9dfc492826d4532e55b44be6b7e34
Sfimadi94 <sydeffect1994@gmail.com>
2019-10-07 23:25:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {id: "asfa", name: 'Max', age: '28' },
+      {id: "sasa", name: 'Manu', age: '29' },
+      {id: "bada", name: 'Stephanie', age: '25' }
+    ],
+    otherState: 'Some Value',
+    showPersons: false
+  };
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: '28' },
+        { name: 'Manu', age: '29' },
+        { name: 'Stephanie', age: '27' }
+      ]
+    });
+  };
+
+  nameChangedHandler = (event, id )=> {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]}
+
+      person.name = event.target.value
+
+      const persons = {...this.state.persons}
+      persons[personIndex] = person
+
+    this.setState({
+      persons: persons
+    });
+  };
+
+  deletePersonsHandler = (personIndex) => {
+    // const persons = this.state.persons.slice()
+
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonsHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className='App'>
+        <h1>Hello</h1>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
